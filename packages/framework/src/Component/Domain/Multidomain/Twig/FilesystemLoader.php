7fce803058e9c0e6dbe6e38086819cb2c49440fb
Rostislav Vitek <rostislav.vitek@shopsys.com>
2019-02-05 15:33:24 +0100

    added custom loader for Twig templates
    - therefore template name can contain designId for using distinct templates on the other domains
    - the custom loader loads templateName.{designId}.html.twig when such a template exists for a given domain instead of the orginal templateName.html.twig

---
+++ packages/framework/src/Component/Domain/Multidomain/Twig/FilesystemLoader.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Shopsys\FrameworkBundle\Component\Domain\Multidomain\Twig;
+
+use Shopsys\FrameworkBundle\Component\Domain\Domain;
+use Symfony\Bundle\TwigBundle\Loader\FilesystemLoader as BaseFilesystemLoader;
+use Symfony\Component\Config\FileLocatorInterface;
+use Symfony\Component\Templating\TemplateNameParserInterface;
+
+class FilesystemLoader extends BaseFilesystemLoader
+{
+    /**
+     * @var \Shopsys\FrameworkBundle\Component\Domain\Domain|null
+     */
+    protected $domain;
+
+    /**
+     * @param \Symfony\Component\Config\FileLocatorInterface $locator
+     * @param \Symfony\Component\Templating\TemplateNameParserInterface $parser
+     * @param string|null $rootPath
+     * @param \Shopsys\FrameworkBundle\Component\Domain\Domain|null $domain
+     */
+    public function __construct(
+        FileLocatorInterface $locator,
+        TemplateNameParserInterface $parser,
+        ?string $rootPath = null,
+        Domain $domain = null
+    ) {
+        $this->domain = $domain;
+        parent::__construct($locator, $parser, $rootPath);
+    }
+
+    /**
+     * When exists a template with filename.{designId}.html.twig, then it is automatically used
+     * on domain with this {designId} whenever template named filename.html.twig is on input
+     *
+     * @inheritdoc
+     */
+    protected function findTemplate($template, $throw = true)
+    {
+        $templateName = (string)$template;
+        if (strpos($templateName, '@ShopsysShop/Front/') === 0) {
+            $this->assertDomainDependency();
+
+            return $this->findMultidesignTemplate($templateName);
+        }
+
+        return parent::findTemplate($template);
+    }
+
+    protected function assertDomainDependency()
+    {
+        if (!($this->domain instanceof Domain)) {
+            $message = sprintf('Template loader needs an instance of %s class', Domain::class);
+            throw new \Shopsys\FrameworkBundle\Component\Domain\Multidomain\Twig\Exception\MissingDependencyException($message);
+        }
+    }
+
+    /**
+     * @param string $templateName
+     * @return string
+     */
+    protected function findMultidesignTemplate($templateName)
+    {
+        $designId = $this->domain->getDesignId();
+        if ($designId !== null) {
+            $multidesignTemplateName = preg_replace('/^(.*)(\.[^\.]*\.twig)/', '$1.' . $designId . '$2', $templateName);
+            try {
+                return parent::findTemplate($multidesignTemplateName);
+            } catch (\Twig_Error_Loader $loaderException) {
+                if (strpos($loaderException->getMessage(), 'Unable to find template') !== 0) {
+                    $message = sprintf('Unexpected exception when trying to load multidesign template `%s`', $multidesignTemplateName);
+                    throw new \Twig_Error_Loader($message, -1, null, $loaderException);
+                }
+            }
+        }
+
+        return parent::findTemplate($templateName);
+    }
+}
